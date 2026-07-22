9c068e1f35567dbbe488bf8be3acfb114e0e5547
Sohil <sohilkhan61843@gmail.com>
2026-07-22 23:54:35 +0530

    Added first Java program

--- Basics/hello.java
+++ Basics/hello.java
@@ -1,8 +1,8 @@
 package Basics;
 
-public class firstcode {
+public class hello {
     public static void main(String[]args){
         System.out.println("hello world");
     }
-    
+
 }
---
+++ Basics/variables.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class variables {
+    public static void main(String[]args){
+        int x = 5;
+        int y=10;
+        int z = x+y;
+
+        System.out.println(z);
+    }
+
+}
