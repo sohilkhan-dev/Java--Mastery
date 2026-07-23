599a1c4e490ff507e8a32bc15826623b2202e5d7
Sohil <sohilkhan61843@gmail.com>
2026-07-23 12:41:59 +0530

    Literal

---
+++ Basics/literals.java
@@ -0,0 +1,20 @@
+package Basics;
+
+public class literals {
+    public static void main(String[] args) {
+        int myNum = 10_00_00;               // Integer (whole number)
+        float myFloatNum = 5.75f;   // Floating point number
+        char myLetter = 'A';  
+        myLetter++;       // Character
+        boolean myBool = false;       // Boolean
+        String myText = "Hello World";     // String
+
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    }
+
+    
+}
