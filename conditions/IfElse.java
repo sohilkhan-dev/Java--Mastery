366db84cd39f5f6ba537b8dfa2421f13a894e78a
Sohil <sohilkhan61843@gmail.com>
2026-07-23 23:40:43 +0530

    learn conditions

---
+++ conditions/IfElse.java
@@ -0,0 +1,16 @@
+package conditions;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        if (x > y) {
+            System.out.println("x is greater than y");
+        } else if (x < y) {
+            System.out.println("x is less than y");
+        } else {
+            System.out.println("x is equal to y");
+        }
+    }
+    
+}
---
+++ conditions/TernaryOperator.java
@@ -0,0 +1,21 @@
+package conditions;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a=4;
+        int result=0;
+        // if(a%2==0){
+        //     result=10;
+        // }
+        // else{
+        //     result=20;
+
+        // }
+        result=(a%2==0)?10:20;  // ternary operator 
+
+
+        System.out.println(result);
+        
+    
+}
+}
