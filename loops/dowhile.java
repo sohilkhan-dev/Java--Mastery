690aa49b142625c4f09f21959481ebfeea6d4ac9
Sohil <sohilkhan61843@gmail.com>
2026-07-24 00:39:08 +0530

    practice loops patterns

---
+++ loops/dowhile.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class dowhile {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println("hii " + i);
+            i++;
+        }while(i<=4);
+        System.out.println("bye  " + i);
+    }
+
+    
+}
---
+++ loops/forloops.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class forloops {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            System.out.println("hii " + i);
+            for(int j=1;j<=3;j++){
+                System.out.println("hello " + j);
+            }
+        }
+        System.out.println("bye  " + 5);
+    }
+
+    
+}
