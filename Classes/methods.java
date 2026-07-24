b2e4c612e41ce79c80b56938282e6746753ade98
Sohil <sohilkhan61843@gmail.com>
2026-07-24 15:01:20 +0530

    practice classes & objects and methods

---
+++ Classes/methods.java
@@ -0,0 +1,15 @@
+package Classes;
+
+public class methods {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+    
+}
---
+++ Classes/objects.java
@@ -0,0 +1,12 @@
+package Classes;
+
+public class objects {
+    public static void main(String[] args) {
+        objects obj = new objects();
+        obj.myMethod();
+    }
+
+    void myMethod() {
+        System.out.println("I just got executed!");
+    }
+}
