f848f99006cfc5f7495caeecf0a34fac989b7aa7
Sohil <sohilkhan61843@gmail.com>
2026-07-26 03:54:10 +0530

    Java: String and StringBuffer

---
+++ String/string.java
@@ -0,0 +1,16 @@
+package String;
+
+public class String{
+    public static void main(String[]args){
+        String name = "navin";
+        System.out.println(name);
+        System.out.println("hello "+name);
+        System.out.println(name.concat(" is a good boy"));
+        System.out.println(name.charAt(0));
+
+
+        
+    
+    
+} 
+}
---
+++ String/stringbuffer.java
@@ -0,0 +1,11 @@
+package String;
+
+public class stringbuffer {
+    public static void main(String[] args) {
+        StringBuffer name = new StringBuffer("navin");
+        System.out.println(name);
+        System.out.println("hello " + name);
+        System.out.println(name.append(" is a good boy"));
+        System.out.println(name.charAt(0));
+    }  
+}       
